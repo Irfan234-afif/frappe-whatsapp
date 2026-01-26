acd478bbee821997406ba5e3eba11625dfb8c040
irfan <apipiirpan@gmail.com>
2026-01-26 21:24:04 +0700

    feat: Add Whatsapp app to the UI with a desktop icon, workspace sidebar, and app screen entry.

--- whatsapp/hooks.py
+++ whatsapp/hooks.py
@@ -11,15 +11,13 @@ app_license = "mit"
 # required_apps = []
 
 # Each item in the list will be shown as an app in the apps page
-# add_to_apps_screen = [
-# 	{
-# 		"name": "whatsapp",
-# 		"logo": "/assets/whatsapp/logo.png",
-# 		"title": "Whatsapp",
-# 		"route": "/whatsapp",
-# 		"has_permission": "whatsapp.api.permission.has_app_permission"
-# 	}
-# ]
+add_to_apps_screen = [
+	{
+		"name": "whatsapp",
+		"title": "Whatsapp",
+		"route": "/whatsapp",
+	}
+]
 
 # Includes in <head>
 # ------------------
